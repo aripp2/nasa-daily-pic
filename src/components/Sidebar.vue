effde1e9ab12809e9d6a922b5ce7e1e1bf5880bf
Amy Rippeto <amyrippeto@gmail.com>
2020-01-13 11:52:35 -0700

    Add Sidebar component with title and subtitle

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <aside>
+    <h1>{{ title }}</h1>
+    <h2>{{ subTitle }}</h2>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: {
+    title: String,
+    subTitle: String
+  }
+}
+</script>
+
+<style scoped>
+  aside {
+    height: 100vh;
+    width: 30%;
+    background: #BDA7D5;
+    margin: 0;
+
+  }
+</style>
