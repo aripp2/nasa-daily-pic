0dfac4461c5f908aa6d4f066a40be46124f0956e
Amy Rippeto <amyrippeto@gmail.com>
2020-01-14 08:23:09 -0700

    Add env file with api key

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import Sidebar from './components/Sidebar.vue'
+import Sidebar from './components/Sidebar.vue';
 import ImageContainer from './components/ImageContainer';
 
 export default {
